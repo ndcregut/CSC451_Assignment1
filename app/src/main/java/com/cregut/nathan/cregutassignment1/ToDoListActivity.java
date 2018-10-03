af7812f6193ccb724e1ad6ada752f913a7bf0d6e
Nathan Cregut <nathan.cregut@trojans.dsu.edu>
2018-10-02 21:28:31 -0500

    Created more views

--- app/src/main/java/com/cregut/nathan/cregutassignment1/ToDoListActivity.java
+++ app/src/main/java/com/cregut/nathan/cregutassignment1/ToDoListActivity.java
@@ -2,16 +2,11 @@ package com.cregut.nathan.cregutassignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ToDoListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    public void goToList(View view) {
-
     }
 }
